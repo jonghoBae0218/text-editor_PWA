b90117ac856e315e96edf78514c599dd4f9a1319
Jongho Bae <jb4918@columbia.edu>
2024-08-28 12:05:26 -0400

    Added putDb functionality

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,38 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // Open the database.
+  const db = await openDB("jate", 1);
+
+  // Open a transaction on the database.
+  const tx = db.transaction("jate", "readwrite");
+
+  // Open the object store.
+  const store = tx.objectStore("jate");
+
+  // Add the data to the object store.
+  const request = store.put({ content, id: 1 });
+
+  // Wait for the database to complete the transaction.
+  const result = await request;
+  console.log("Data saved to the database", result);
+  return result;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => console.error("getDb not implemented");
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,7 +2,20 @@ const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener("beforeinstallprompt", (event) => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  // Show the install button
+  butInstall.classList.remove("hidden");
+
+  // Event handler for the `butInstall` element
+  butInstall.addEventListener("click", () => {
+    // Show the install prompt
+    event.prompt();
+
+    // Hide the install button
+    butInstall.classList.add("hidden");
+  });
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener("click", async () => {});
